4a9143d11f2a42cf75c86843ef40ea98526d0485
innocent-soul <70508632+innocent-soul@users.noreply.github.com>
2021-03-01 19:13:09 +0530

    Create Two Sum.py

---
+++ Day-7/Two Sum.py
@@ -0,0 +1,9 @@
+# availiable on leetcode
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # simple brute force  
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
